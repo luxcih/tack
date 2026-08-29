db9adb1f9c4284994dc5f3d5e9c15e11436985b6
Luxcih <luxcih.me@gmail.com>
2026-08-29 13:44:50 +0800

    Allow application state in behavior context

--- src/Context.zig
+++ src/Context.zig
@@ -8,10 +8,18 @@ allocator: std.mem.Allocator,
 cli: *const CLI,
 output: ?*std.Io.Writer = null,
 
+/// Application-defined state available to actions and behaviors.
+data: ?*anyopaque = null,
+
 pub const OutputError = error{
     OutputUnavailable,
 };
 
 pub fn writer(self: *Context) OutputError!*std.Io.Writer {
     return self.output orelse error.OutputUnavailable;
+}
+
+pub fn dataAs(self: *const Context, comptime T: type) ?*T {
+    const data = self.data orelse return null;
+    return @ptrCast(@alignCast(data));
 }
