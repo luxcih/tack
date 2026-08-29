e424134bb9c077de9d749b9c52f48bac8570cb67
Luxcih <luxcih.me@gmail.com>
2026-08-29 14:34:54 +0800

    Remove leftover help abstraction

--- src/root.zig
+++ src/root.zig
@@ -3,7 +3,3 @@ pub const CLI = @import("CLI.zig");
 pub const Command = @import("Command.zig");
 pub const Invocation = @import("Invocation.zig");
 pub const Parser = @import("Parser.zig");
-
-pub const Help = @import("Help.zig");
-
-pub const help_option = Help.option;
