e263f16bbe027f97190a36d0afcd7715056204cf
Luxcih <luxcih.me@gmail.com>
2026-08-29 11:19:31 +0800

    Add CLI run method

--- src/CLI.zig
+++ src/CLI.zig
@@ -1,6 +1,9 @@
+const std = @import("std");
+
 const Action = @import("Action.zig");
 const Command = @import("Command.zig");
 const Invocation = @import("Invocation.zig");
+const Parser = @import("Parser.zig");
 
 const CLI = @This();
 
@@ -12,6 +15,17 @@ options: []const Command.Option = &.{},
 commands: []const Command = &.{},
 
 action: ?Action = null,
+
+pub fn run(
+    self: *const CLI,
+    allocator: std.mem.Allocator,
+    args: []const []const u8,
+) !void {
+    var invocation = try Parser.parse(allocator, self, args);
+    defer invocation.deinit(allocator);
+
+    try self.dispatch(&invocation);
+}
 
 pub fn dispatch(self: *const CLI, invocation: *const Invocation) !void {
     _ = self;
